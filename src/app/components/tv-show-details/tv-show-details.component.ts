bc567b68597a977c537456ef1237e75a3ff87099
felipe <felipedemaria@hotmail.com>
2021-01-20 17:01:52 -0300

    Fixed the tv show details component to display the image and the videos url (now safe urls) correctly

--- src/app/components/tv-show-details/tv-show-details.component.ts
+++ src/app/components/tv-show-details/tv-show-details.component.ts
@@ -3,6 +3,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { TvShowDetailsService } from '../../services/tv-show-details.service';
 import { TvShowDetails } from '../../interfaces/TvShowDetails';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tv-show-details',
@@ -11,22 +12,31 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class TvShowDetailsComponent implements OnInit {
 
+  safeUrlVideos : SafeResourceUrl[] = [];
   isLoading : boolean;
   tvShowDetails = {} as TvShowDetails;
   constructor(@Inject(MAT_DIALOG_DATA) public idTvShow : number,
-              private tvShowDetailsService: TvShowDetailsService) { }
+              private tvShowDetailsService: TvShowDetailsService,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.isLoading = true;
     this.getTvShowDetails(this.idTvShow);
   }
 
+  // We get the tv show details from our service, sanitized the trailers url, and then set the loading flag to false.
   getTvShowDetails(idTvShow: number): void {
     this.tvShowDetailsService.getTvShowDetailsById(idTvShow).subscribe((tvShowsResult) => {
     this.tvShowDetails = tvShowsResult;
-    console.log(this.tvShowDetails);
+    this.sanitizeVideosUrl();
     this.isLoading = false;
     });
   }
 
+  // We iterate the url from the details object, and populate each new secured url into the safeUrlVideos array.
+  sanitizeVideosUrl() : void {
+    if(this.tvShowDetails.trailersURL.length > 0){
+      this.tvShowDetails.trailersURL.forEach((videos) => this.safeUrlVideos.push(this.sanitizer.bypassSecurityTrustResourceUrl(videos)));
+    }
+  }
 }
